da6b5f9313f28165d5aaee20703419e76911d51b
Unknown <elaric@ngs.ru>
2019-04-22 10:26:34 +0700

    0.0.2
    work with stolp distribution

--- FrisLib.py
+++ FrisLib.py
@@ -1,57 +1,102 @@
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
-
+import numpy as np
 
 class LearnClass:
     pass
 
 
 class FrisCluster:
-    def __init__(self, K, VirtualM=1):
+    def __init__(self, K, distance='euclidean', virtual_M=1):
         self.__K = K
-        self.__virtual_m = VirtualM
-        self.__StolpList = pd.DataFrame()
+        self.__virtual_m = virtual_M
+        self.__stolp_list = []
+        if distance == 'euclidean':
+            self.m = lambda x, y: np.linalg.norm(x - y)
+
+    def find_stolp(self, X, stolp_list=[]):
+        max_f = -100000
+        max_index = None
+        for index, row in X.iterrows():
+            # print(row)
+            # print(stolp_list)
+            if row.values not in np.array(stolp_list):
+                cur_stolp_list = stolp_list + [row.values]
+                # print(cur_stolp_list)
+                cur_f = self.sum_virtual_f(X, cur_stolp_list)
+                if cur_f > max_f:
+                    max_f = cur_f
+                    max_index = index
+        stolp_list.append(X.iloc[max_index].values)
+        return stolp_list
+
+    def distribute_stolp_list(self, stolp_list, data):
+        nbrs = NearestNeighbors(n_neighbors=1, metric='euclidean').fit(stolp_list)
+        nn = nbrs.kneighbors(data.values, n_neighbors=1, return_distance=False)
+        cluster = data.apply(lambda x: nn[x.name][0], axis=1)
+        for cl in cluster.unique():
+            cur_cluster = data[data.index.isin(cluster[cluster == cl].index)]
+            cur_cluster_stolp = stolp_list[cl]
+            stolp_list = list(filter(lambda x: (x != cur_cluster_stolp).all(),stolp_list))
+            cur_cluster_F = cur_cluster.apply(lambda x: self.sum_f_cluster(x, cur_cluster,stolp_list),axis=1)
+            maxrow = cur_cluster.loc[cur_cluster_F.idxmax()].values
+            print("old stolp: {}. new stolp:{}".format(cur_cluster_stolp,maxrow ))
+            stolp_list.append(maxrow)
+        return  stolp_list
 
     def fit(self, X):
 
-        stolp_index = None
-        max_f = -1
-        for index, row in X.iterrows():
-            f = self.sum_virtual_f(row)
-            if f > max_f:
-                max_f = f
-                stolp_index = index
-            self.__StolpList = pd.DataFrame()
+        stolp_list = self.find_stolp(X)
+        stolp_list = self.find_stolp(X, stolp_list)
+        print(stolp_list)
+        for k in range(2,self.__K):
+            stolp_list = self.find_stolp(X, stolp_list)
+        self.__stolp_list = stolp_list
 
-        a = X.sample(n=1, weights='num_specimen_seen', random_state=1)
-        for k in range(self.__K):
-
-
+    def get_stolp_list(self):
+        return self.__stolp_list
 
     def predict(self, X, Y):
         pass
 
-    def m(self,x,y):
+    def m(self, x, y):
         pass
 
     def nearest_stolp(self, x, stolp_list=None):
-        if stolp_list is None:
-            stolp_list = self.__StolpList
-        nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(stolp_list)
-        nn = nbrs.kneighbors(x, n_neighbors=1, return_distance=False)
-        return self.__StolpList[nn[0]]
+        nbrs = NearestNeighbors(n_neighbors=1, metric='euclidean').fit(stolp_list)
+        nn = nbrs.kneighbors([x], n_neighbors=1, return_distance=False)
+        # print(nn[0][0])
+        # print(stolp_list)
+        return stolp_list[nn[0][0]]
 
+    def FRIS(self,a, s_a1, s_a2):
+        return (self.m(s_a2, a) - self.m(s_a1, a)) / (self.m(s_a2, a) + self.m(s_a1, a))
 
-    def virtual_f(self,a, stolp_list=None):
+    def virtual_f(self, a, stolp_list=None):
         s_a1 = self.nearest_stolp(a, stolp_list)
-        return (self.m(a,s_a1) - self.VirtualF) / (self.m(a,s_a1) + self.VirtualF)
+        return ( self.__virtual_m  - self.m(a, s_a1)) / (self.__virtual_m + self.m(a, s_a1))
 
 
-    def sum_virtual_f(self, a):
-        return a.apply(self.virtual_f).sum()
+    def f(self, a, stolp_list=None):
+        s_a1 = self.nearest_stolp(a, stolp_list)
+        s_a2 = self.nearest_stolp(a, list(filter(lambda x: (x != s_a1).all(),stolp_list)))
+        return self.FRIS(a, s_a1, s_a2) #(self.m(s_a2, a) - self.m(s_a1, a)) / (self.m(s_a2, a) + self.m(s_a1, a))
 
+    def sum_virtual_f(self, a, stolp_list):
+        M = a.shape[0]
+        return a.apply(lambda x: self.virtual_f(x.values, stolp_list), axis=1).sum()/M
 
+    def sum_f_cluster(self, cluster_stolp, cluster_data,stolp_list):
+        M = cluster_data.shape[0]
+        return cluster_data.apply(lambda x: self.cluster_f(x, cluster_stolp, stolp_list), axis=1).sum()/M
 
+    def cluster_f(self, a, cluster_stolp, stolp_list):
+        enemy_stolp = self.nearest_stolp(a,stolp_list)
+        return self.FRIS(a, cluster_stolp, enemy_stolp)
+
+    def sum_f(self, a, stolp_list=None):
+        M = a.shape[0]
+        return a.apply(lambda x: self.f(x.values, stolp_list), axis=1).sum()/M
 
 class FrisClass:
     pass
